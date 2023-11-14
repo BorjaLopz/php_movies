0616097006f8916ebf48ff888c09111c7b2642bf
Borja <lopzdiazborja@gmail.com>
2023-11-14 01:15:25 +0100

    Terminamos de maquetar web

--- php_movies_front/src/Components/MisPeliculasComponent/MisPeliculasComponent.jsx
+++ php_movies_front/src/Components/MisPeliculasComponent/MisPeliculasComponent.jsx
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 
 function MisPeliculasComponent() {
   const [databaseMovies, setDatabaseMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchDatabaseMovies = async () => {
     try {
@@ -19,9 +20,10 @@ function MisPeliculasComponent() {
 
   useEffect(() => {
     fetchDatabaseMovies();
-  }, []);
+  }, [isLoading]);
 
   const handleDeleteMovie = (movieInfo) => {
+    setIsLoading(true);
     // Configuración de la solicitud DELETE
     const reqOptions = {
       method: "DELETE",
@@ -43,6 +45,7 @@ function MisPeliculasComponent() {
         }
         console.log("Película eliminada correctamente");
         toast.success("Película eliminada correctamente");
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error("Error al realizar la solicitud DELETE:", error);
@@ -53,26 +56,29 @@ function MisPeliculasComponent() {
 
   return (
     <main className="Main">
-      <h1>Mis películas</h1>
+      {/* <h1>Mis películas</h1> */}
       <section className="listadoPeliculas">
-        {databaseMovies.map((movie) => (
-          <article key={movie._id} className="pelicula">
-            <div className="movieInfo">
-              <h3>{movie.title}</h3>
-              <h3>{movie.rating}</h3>
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${movie.imagen}`}
-                alt=""
-              />
-              <button
-                id="deleteMovieButton"
-                onClick={() => handleDeleteMovie(movie)}
-              >
-                Borrar película
-              </button>
-            </div>
-          </article>
-        ))}
+        {isLoading && <h1>Cargando...</h1>}
+        {databaseMovies.length === 0 ? <h2>No has agregado aún ninguna pelicula</h2> : ""}
+        {!isLoading && databaseMovies.length > 0 && 
+          databaseMovies.map((movie) => (
+            <article key={movie._id} className="pelicula">
+              <div className="movieInfo">
+                <h3>{movie.title}</h3>
+                <h3>{movie.rating}</h3>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.imagen}`}
+                  alt=""
+                />
+                <button
+                  id="deleteMovieButton"
+                  onClick={() => handleDeleteMovie(movie)}
+                >
+                  Borrar película
+                </button>
+              </div>
+            </article>
+          ))}
       </section>
     </main>
   );
